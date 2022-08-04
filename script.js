6b0ff2a7a5fbc850ec9858ce2202aae19053caed
Nuttadon <nuttadon_p@cmu.ac.th>
2022-08-04 15:31:41 +0700

    try

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const pname = document.getElementById("p-name");
 const paddress = document.getElementById("p-address");
 const pemail = document.getElementById("p-email");
 const icon = document.getElementById("span-gender-icon");
+const load = document.getElementById("div-loading-card");
+const usercard = document.getElementById("div-user-card");
 
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
@@ -22,23 +24,17 @@ async function callApi() {
   pemail.innerText = resp.data.results[0].email;
   if (resp.data.results[0].gender === "female") icon.innerText = "👩";
   if (resp.data.results[0].gender === "male") icon.innerText = "👨";
+  usercard.style.display = "";
+  load.style.display = "none";
 }
 callApi();
 
-btnRandom.onClick = async () => {
-  const resp = await axios.get("https://randomuser.me/api/");
-  imgProfile.src = resp.data.results[0].picture.large;
-  pname.innerText =
-    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
-  paddress.innerText =
-    resp.data.results[0].location.city +
-    " " +
-    resp.data.results[0].location.state +
-    " " +
-    resp.data.results[0].location.country +
-    " " +
-    resp.data.results[0].location.postcode;
-  pemail.innerText = resp.data.results[0].email;
-  if (resp.data.results[0].gender === "female") icon.innerText = "👩";
-  if (resp.data.results[0].gender === "male") icon.innerText = "👨";
+btnRandom.onclick = async () => {
+  callApi();
+  load.style.display = "";
+  () => {
+    load.style.display = "none";
+  },
+    1000;
+  usercard.style.display = "none";
 };
