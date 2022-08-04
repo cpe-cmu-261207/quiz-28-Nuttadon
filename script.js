d0ccf554b7ec5bc9a7878472132e1d438c3bfdd6
Nuttadon <nuttadon_p@cmu.ac.th>
2022-08-04 14:15:40 +0700

    gg

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+async function callApi() {
+  const resp = await axios.get("https://randomuser.me/api/");
+  imgProfile.src = resp.data.results[0].picture.large;
+  pname.innerText = resp.data.results[0].name;
+  paddress.innerText = resp.data.results[0].country;
+  pemail.innerText = resp.data.results[0].email;
+}
+callApi();
 const imgProfile = document.getElementById("img-profile");
 const btnRandom = document.querySelector("#btn-random");
 const divLoadingCard = document.getElementById("div-loading-card");
