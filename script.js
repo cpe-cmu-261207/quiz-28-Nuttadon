f93be6477ee791cf1b0ee70c232780ed1250acb2
Nuttadon <nuttadon_p@cmu.ac.th>
2022-08-04 14:12:22 +0700

    HH

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-async function callApi() {
+const imgProfile = document.getElementById("img-profile");
+const btnRandom = document.querySelector("#btn-random");
+const divLoadingCard = document.getElementById("div-loading-card");
+const pname = document.getElementById("p-name");
+const paddress = document.getElementById("p-address");
+const pemail = document.getElementById("p-email");
+
+btnRandom.onClick = async () => {
+  btnRandom.innerText = "Loading...";
+  btnRandom.disabled = true;
   const resp = await axios.get("https://randomuser.me/api/");
-}
+  imgProfile.src = resp.data.results[0].picture.large;
+  pname.innerText = resp.data.results[0].name;
+  paddress.innerText = resp.data.results[0].country;
+  pemail.innerText = resp.data.results[0].email;
+};
