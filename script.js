32d6ca8902c2fdb0c93ccc00a067a70ba03a5d84
Nuttadon <nuttadon_p@cmu.ac.th>
2022-08-04 14:23:23 +0700

    ahhh

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
   imgProfile.src = resp.data.results[0].picture.large;
-  pname.innerText = resp.data.results[0].name;
-  paddress.innerText = resp.data.results[0].country;
+  pname.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  paddress.innerText =
+    resp.data.results[0].location.city +
+    " " +
+    resp.data.results[0].location.state +
+    " " +
+    resp.data.results[0].location.country +
+    " " +
+    resp.data.results[0].location.postcode;
   pemail.innerText = resp.data.results[0].email;
 }
 callApi();
@@ -13,12 +21,6 @@ const pname = document.getElementById("p-name");
 const paddress = document.getElementById("p-address");
 const pemail = document.getElementById("p-email");
 
-btnRandom.onClick = async () => {
-  btnRandom.innerText = "Loading...";
-  btnRandom.disabled = true;
-  const resp = await axios.get("https://randomuser.me/api/");
-  imgProfile.src = resp.data.results[0].picture.large;
-  pname.innerText = resp.data.results[0].name;
-  paddress.innerText = resp.data.results[0].country;
-  pemail.innerText = resp.data.results[0].email;
+btnRandom.onClick = () => {
+  callApi();
 };
